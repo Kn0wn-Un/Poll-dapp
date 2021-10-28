86c6f8fa78bfcdfef0ebce5aa4fbf0ab4202738a
Darshan H M <naikdarsh175@gmail.com>
2021-10-28 23:34:22 +0530

    modifiers added

--- contracts/PollApp.sol
+++ contracts/PollApp.sol
@@ -18,14 +18,14 @@ contract PollApp {
     Poll[] public pollsArray;
 
     // user vote data structure
-    struct UserVote {
+    struct Vote {
         uint256 poll;
         uint8 option;
     }
 
     // mapping user to votes
     // mappping poll to users
-    mapping(address => UserVote[]) public userVotes;
+    mapping(address => Vote[]) public userVotes;
     mapping(uint256 => address[]) public pollVotes;
 
     // event poll created
@@ -33,26 +33,55 @@ contract PollApp {
     event PollCreated(address indexed creator, uint256 pollNumber);
     event UserVoted(address indexed user, uint256 pollNumber, uint8 option);
 
-    // function createPoll to create new polls and push it into pollsArray
-    function createPoll(
+    // modifier to check if _pollNumber is valid
+    modifier pollExists(uint256 _pollNumber) {
+        require(_pollNumber < pollsArray.length, "Invalid poll number");
+        _;
+    }
+
+    // modifier to check if _question, _timeLimit and _options are valid
+    modifier pollCheck(
         string memory _question,
-        uint8 _time,
-        uint8 _numberOfOptions,
+        uint8 _timeLimit,
         string[] memory _options
-    ) public {
+    ) {
         require(
             keccak256(abi.encode(_question)) != keccak256(abi.encode("")),
             "Question is empty"
         );
-        require(_time <= 24, "Time cannot exceed 24 hours");
+        require(_timeLimit <= 24, "Time cannot exceed 24 hours");
         require(_options.length != 1, "Only one option given");
+        _;
+    }
+
+    // modifier to check if _optionNumber and voting time is valid
+    modifier voteCheck(uint256 _pollNumber, uint8 _optionNumber) {
+        require(
+            _optionNumber < pollsArray[_pollNumber].numberOfOptions,
+            "Invalid option number"
+        );
+        Poll memory poll = pollsArray[_pollNumber];
+        require(
+            block.timestamp <= poll.timeCreated + poll.timeLimit * 1 hours,
+            "Time limit exceeded"
+        );
+        _;
+    }
+
+    // function createPoll to create new polls and push it into pollsArray
+    function createPoll(
+        string memory _question,
+        uint8 _timeLimit,
+        uint8 _numberOfOptions,
+        string[] memory _options
+    ) public pollCheck(_question, _timeLimit, _options) {
         if (_options.length != _numberOfOptions) {
             _numberOfOptions = uint8(_options.length);
         }
         Poll memory poll;
         poll.question = _question;
         poll.creator = msg.sender;
-        poll.timeLimit = _time;
+        poll.timeLimit = _timeLimit;
         poll.numberOfOptions = _numberOfOptions;
         poll.options = _options;
         poll.timeCreated = block.timestamp;
@@ -63,26 +92,18 @@ contract PollApp {
     }
 
     // function vote to allow user to vote in a poll
-    function vote(uint256 _pollNumber, uint8 _optionNumber) public {
-        require(_pollNumber < pollsArray.length, "Invalid poll number");
-        require(
-            _optionNumber < pollsArray[_pollNumber].numberOfOptions,
-            "Invalid option number"
-        );
-        require(
-            block.timestamp <=
-                pollsArray[_pollNumber].timeCreated +
-                    pollsArray[_pollNumber].timeLimit *
-                    1 hours,
-            "Time limit exceeded"
-        );
+    function vote(uint256 _pollNumber, uint8 _optionNumber)
+        public
+        pollExists(_pollNumber)
+        voteCheck(_pollNumber, _optionNumber)
+    {
         for (uint256 i = 0; i < pollVotes[_pollNumber].length; i++) {
             if (pollVotes[_pollNumber][i] == msg.sender)
                 revert("Already voted");
         }
         Poll storage poll = pollsArray[_pollNumber];
         poll.votes[_optionNumber]++;
-        userVotes[msg.sender].push(UserVote(_pollNumber, _optionNumber));
+        userVotes[msg.sender].push(Vote(_pollNumber, _optionNumber));
         pollVotes[_pollNumber].push(msg.sender);
         emit UserVoted(msg.sender, _pollNumber, _optionNumber);
     }
@@ -96,9 +117,9 @@ contract PollApp {
     function getVotes(uint256 _pollNumber)
         public
         view
+        pollExists(_pollNumber)
         returns (uint256[] memory votes)
     {
-        require(_pollNumber < pollsArray.length, "Invalid poll number");
         votes = pollsArray[_pollNumber].votes;
     }
 
@@ -106,7 +127,7 @@ contract PollApp {
     function getUserVotes(address _userAddress)
         public
         view
-        returns (UserVote[] memory votes)
+        returns (Vote[] memory votes)
     {
         votes = userVotes[_userAddress];
     }
@@ -115,9 +136,9 @@ contract PollApp {
     function getPollVotedUsers(uint256 _pollNumber)
         public
         view
+        pollExists(_pollNumber)
         returns (address[] memory users)
     {
-        require(_pollNumber < pollsArray.length, "Invalid poll number");
         users = pollVotes[_pollNumber];
     }
 
@@ -125,10 +146,10 @@ contract PollApp {
     function getUserPollVote(uint256 _pollNumber, address _userAddress)
         public
         view
-        returns (UserVote memory userVote)
+        pollExists(_pollNumber)
+        returns (Vote memory userVote)
     {
-        require(_pollNumber < pollsArray.length, "Invalid poll number");
-        UserVote[] memory votes = userVotes[_userAddress];
+        Vote[] memory votes = userVotes[_userAddress];
         bool hasVoted = false;
         for (uint256 i = 0; i < votes.length; i++) {
             if (votes[i].poll == _pollNumber) {
