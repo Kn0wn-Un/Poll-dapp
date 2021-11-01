2a74d40a6843289acf52963aacd542b43f46fddc
Darshan H M <naikdarsh175@gmail.com>
2021-11-02 00:08:46 +0530

    pollapp web3 test

--- client/src/App.js
+++ client/src/App.js
@@ -1,73 +1,95 @@
-import React, { Component } from 'react';
-import SimpleStorageContract from './contracts/SimpleStorage.json';
+import React, { useState, useEffect } from 'react';
+// import SimpleStorageContract from './contracts/SimpleStorage.json';
+import PollApp from './contracts/PollApp.json';
 import getWeb3 from './getWeb3';
 
 import './App.css';
 
-class App extends Component {
-	state = { storageValue: 0, web3: null, accounts: null, contract: null };
+function App() {
+	const [res, setRes] = useState(0);
+	const [web3, setWeb3] = useState(null);
+	const [accounts, setAccounts] = useState(null);
+	const [contract, setContract] = useState(null);
 
-	componentDidMount = async () => {
-		try {
-			// Get network provider and web3 instance.
-			const web3 = await getWeb3();
-
-			// Use web3 to get the user's accounts.
-			const accounts = await web3.eth.getAccounts();
-
-			// Get the contract instance.
-			const networkId = await web3.eth.net.getId();
-			const deployedNetwork = SimpleStorageContract.networks[networkId];
-			const instance = new web3.eth.Contract(
-				SimpleStorageContract.abi,
-				deployedNetwork && deployedNetwork.address
-			);
-
-			// Set web3, accounts, and contract to the state, and then proceed with an
-			// example of interacting with the contract's methods.
-			this.setState({ web3, accounts, contract: instance }, this.runExample);
-		} catch (error) {
-			// Catch any errors for any of the above operations.
-			alert(
-				`Failed to load web3, accounts, or contract. Check console for details.`
-			);
-			console.error(error);
-		}
-	};
-
-	runExample = async () => {
-		const { accounts, contract } = this.state;
-
+	const getPolls = async (c) => {
 		// Stores a given value, 5 by default.
-		await contract.methods.set(69).send({ from: accounts[0] });
-
-		// Get the value from the contract to prove it worked.
-		const response = await contract.methods.get().call();
+		const response = await c.methods.getAllPolls().call();
 
 		// Update state with the result.
-		this.setState({ storageValue: response });
+		setRes(response);
 	};
 
-	render() {
-		if (!this.state.web3) {
-			return <div>Loading Web3, accounts, and contract...</div>;
+	const makePoll = async () => {
+		// Stores a given value, 5 by default.
+		await contract.methods
+			.createPoll('Question?', 1, 2, ['yes', 'no'])
+			.send({ from: accounts[0] });
+	};
+
+	useEffect(() => {
+		(async () => {
+			try {
+				// Get network provider and web3 instance.
+				const w3 = await getWeb3();
+
+				// Use web3 to get the user's accounts.
+				const acc = await w3.eth.getAccounts();
+
+				// Get the contract instance.
+				const networkId = await w3.eth.net.getId();
+				const deployedNetwork = PollApp.networks[networkId];
+				const instance = new w3.eth.Contract(
+					PollApp.abi,
+					deployedNetwork && deployedNetwork.address
+				);
+
+				instance.events
+					.PollCreated()
+					.on('data', function (event) {
+						getPolls(instance);
+						console.log(event);
+					})
+					.on('error', console.error);
+
+				// Set web3, accounts, and contract to the state, and then proceed with an
+				// example of interacting with the contract's methods.
+				setWeb3(w3);
+				setAccounts(acc);
+				setContract(instance);
+				getPolls(instance);
+			} catch (error) {
+				// Catch any errors for any of the above operations.
+				alert(
+					`Failed to load web3, accounts, or contract. Check console for details.`
+				);
+				console.error(error);
+			}
+		})();
+	});
+	useEffect(() => {
+		if (contract) {
+			getPolls(contract);
 		}
-		return (
-			<div className="App">
-				<h1>Good to Go!</h1>
-				<p>Your Truffle Box is installed and ready.</p>
-				<h2>Smart Contract Example</h2>
-				<p>
-					If your contracts compiled and migrated successfully, below will show
-					a stored value of 69 (by default).
-				</p>
-				<p>
-					Try changing the value stored on <strong>line 42</strong> of App.js.
-				</p>
-				<div>The stored value is: {this.state.storageValue}</div>
-			</div>
-		);
+	}, [contract]);
+	if (!web3) {
+		return <div>Loading Web3, accounts, and contract...</div>;
 	}
+	return (
+		<div className="App">
+			<h1>Good to Go!</h1>
+			<p>Your Truffle Box is installed and ready.</p>
+			<h2>Smart Contract Example</h2>
+			<p>
+				If your contracts compiled and migrated successfully, below will show a
+				stored value of 69 (by default).
+			</p>
+			<p>
+				Try changing the value stored on <strong>line 42</strong> of App.js.
+			</p>
+			<div>The array is of length: {res.length}</div>
+			<button onClick={makePoll}>Make Poll!</button>
+		</div>
+	);
 }
 
 export default App;
